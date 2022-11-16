fbf44777b0a82bc6c5ce670ba4ddb094b99bdffe
krst221 <krst221@gmail.com>
2022-11-16 20:42:50 +0100

    enviar tweets

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
     $tweets = file_get_contents('tweets.json');
-    $arraytweets = json_decode($tweets, true);
-    require_once  "index.html";
+    $arrayTweets = json_decode($tweets, true);
+    $tweettext = $_POST['tweettext'];
+
+    if(isset($_POST['tweetButton']) && $tweettext){
+        $tweet = [
+            'texto' => $tweettext,
+            'autor' => 'Chris',
+            'username' => 'JesusChrist'
+        ];
+        array_unshift($arrayTweets['tweets'], $tweet);
+        file_put_contents('tweets.json', json_encode($arrayTweets, JSON_PRETTY_PRINT));
+    }
+    require_once "index.html";
 ?>
