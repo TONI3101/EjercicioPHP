e3eaabdb2eeebda8e234da6eb6f042f104e783da
unknown <alberto.garcia@bootcamp-upgrade.com>
2022-11-16 16:06:39 +0100

    Second commit

--- index.php
+++ index.php
@@ -1,7 +1,18 @@
 <?php
-$tweet = ['Author' => 'IndioFullstack', 
-'Username' => '@IndioFullStack', 
-'Text' => 'El indio explicandote como hacer twitter'];
+
+$tweets = [
+    ['Author' => 'ElonMusk', 
+'Username' => '@ElonMusk', 
+'Text' => 'Pay me Please'],
+
+['Author' => 'Blangaguy', 
+'Username' => '@Banglaguy', 
+'Text' => 'The longest Day'],
+
+['Author' => 'Diostwitero', 
+'Username' => '@Diostwitero', 
+'Text' => 'Ya tengo cuenta con check azul y sin pagar'],
+];
 
 require_once  "index.html";
 ?>
